f301c3b19995a803556add31f5a1e08243370acc
JSONRete <jsonrete@gmail.com>
2021-10-14 04:37:10 -0400

    commit tree e5aa84d71f11bd00d4cd085d3ff718ed302c142a

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+
+    switch (action.type) {
+        case "ADD_FRIEND":
+          return ({   ...state, friends: [ ...state.friends, action.friend] })
+        case "REMOVE_FRIEND":
+        //   return { REMOVE_FRIEND: state.REMOVE_FRIEND - 1 };
+        default:
+          return state;
+      }
 }
+  
+  let state = { friends: [] };
